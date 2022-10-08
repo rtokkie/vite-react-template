ee7a2fb90a0e71a0574af954b21316c9e3d04049
rtokkie <natto.and.eggs@gmail.com>
2022-10-08 14:02:41 +0900

    @ import setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite'
+
+// SEE: https://antfu.me/posts/isomorphic-dirname
+const _dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': `${_dirname}/src/`,
+    },
+  },
 })
